6f46b32f594bf6b3d4940665d5d502f914e20ad3
Li Yang <sdshiyan2005@gmail.com>
2014-04-16 16:43:41 +0800

    ver 0.1 package

---
+++ R/EIPs.R
@@ -0,0 +1,3 @@
+EIPs<-function(){
+  cat("This is a package that contains network data and functions for Eco-Industrial Parks(EIPs).\n Use data(\"eipdatasource\") to see EIP names and data sources.")
+}
